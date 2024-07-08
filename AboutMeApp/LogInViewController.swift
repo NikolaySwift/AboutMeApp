41796186d437a85d6024ec0093bfe6f136ff6ec9
NikolayD <volodar86@yandex.ru>
2024-07-08 19:58:02 +0300

    create func for showing alert. call it in two buttons forgot name and forgot password to show message. override touchesBegan method. override shouldPerformSegue where input login and password are checked. override method prepare to send user name to the welcome view controller. create two private property with correct login and password.

--- AboutMeApp/LogInViewController.swift
+++ AboutMeApp/LogInViewController.swift
@@ -12,16 +12,77 @@ final class LogInViewController: UIViewController {
     @IBOutlet var userNameTF: UITextField!
     @IBOutlet var passwordTF: UITextField!
     
+    private let correctUserName = "User"
+    private let correctPassword = "1234"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let welcomeVC = segue.destination as? WelcomeViewController
+        welcomeVC?.userName = userNameTF.text
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        view.endEditing(true)
+    }
+    
+    override func shouldPerformSegue(
+        withIdentifier identifier: String,
+        sender: Any?
+    ) -> Bool {
+        guard let userName = userNameTF.text, let password = passwordTF.text else {
+            return false
+        }
+        
+        guard !userName.isEmpty, !password.isEmpty else {
+            showAlert(
+                WithTitle: "Invalid login or password",
+                AndMessage: "please enter correct login and password"
+            )
+            return false
+        }
+
+        guard userName == correctUserName, password == correctPassword else {
+            showAlert(
+                WithTitle: "Invalid login or password",
+                AndMessage: "please enter correct login and password"
+            )
+            return false
+        }
+        
+        return true
+    }
 
     @IBAction func forgotUserNameButtonTapped() {
+        showAlert(
+            WithTitle: "Oops!",
+            AndMessage: "Your name is \(correctUserName)"
+        )
     }
     
     @IBAction func forgotPasswordButtonTapped() {
+        showAlert(
+            WithTitle: "Oops!",
+            AndMessage: "Your password is \(correctPassword)"
+        )
     }
     
 }
 
+extension LogInViewController {
+    private func showAlert(WithTitle title: String, AndMessage message: String) {
+        let alert = UIAlertController(
+            title: title,
+            message: message,
+            preferredStyle: .alert
+        )
+        
+        let okAction = UIAlertAction(title: "OK", style: .default)
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
+}
