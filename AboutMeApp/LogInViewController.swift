f7f1f7cccd5e7813ff3cf0ce93e6624c8b8ed4bf
NikolayD <volodar86@yandex.ru>
2024-07-08 18:24:15 +0300

    link elements from storyboard with IBOutlets in view controllers. create action method for log out button.

--- AboutMeApp/LogInViewController.swift
+++ AboutMeApp/LogInViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 final class LogInViewController: UIViewController {
 
+    @IBOutlet var userNameTF: UITextField!
+    @IBOutlet var passwordTF: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
 
-
+    @IBAction func forgotUserNameButtonTapped() {
+    }
+    
+    @IBAction func forgotPasswordButtonTapped() {
+    }
+    
 }
 
