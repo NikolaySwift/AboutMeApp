47f9fd785af14884fb42ae6b70730081ef2ca83b
NikolayD <volodar86@yandex.ru>
2024-07-08 18:22:09 +0300

    add on the first screen two text fields, button for log in and two buttons for tips, put them into stack. Add second view controller, add label with welcome text and log out button. rename first view controller to LoginViewController and create WelcomeViewController for second screen. Create segue.

--- AboutMeApp/LogInViewController.swift
+++ AboutMeApp/LogInViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-final class ViewController: UIViewController {
+final class LogInViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
