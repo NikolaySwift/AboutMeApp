b2900857ac7db4ab9cf10abe1303fd8044990b9d
NikolayD <volodar86@yandex.ru>
2024-07-12 20:30:45 +0300

    create DetailsViewController

--- AboutMeApp/Controllers/DetailsViewController.swift
+++ AboutMeApp/Controllers/DetailsViewController.swift
@@ -7,23 +7,38 @@
 
 import UIKit
 
-class DetailsViewController: UIViewController {
+final class DetailsViewController: UIViewController {
 
+    @IBOutlet var detailTitleNavigationItem: UINavigationItem!
+    @IBOutlet var descriptionLabel: UILabel!
+    
+    @IBOutlet var backButton: UIButton!
+    @IBOutlet var nextButton: UIButton!
+    
+    var details: [Detail]!
+    private var detailIndex = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        setupButton()
+        setText()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        detailIndex += sender == nextButton ? 1 : -1
+        
+        setupButton()
+        setText()
     }
-    */
-
+    
+    private func setupButton() {
+        backButton.isEnabled = detailIndex > 0
+        nextButton.isEnabled = detailIndex + 1 < details.count
+    }
+    
+    private func setText() {
+        detailTitleNavigationItem.title = details[detailIndex].title
+        descriptionLabel.text = details[detailIndex].description
+    }
 }
