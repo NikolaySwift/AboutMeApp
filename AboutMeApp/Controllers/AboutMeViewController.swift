eb866891e31a304684013e72602386821cddc20d
NikolayD <volodar86@yandex.ru>
2024-07-12 20:31:30 +0300

    create AboutMeViewController

--- AboutMeApp/Controllers/AboutMeViewController.swift
+++ AboutMeApp/Controllers/AboutMeViewController.swift
@@ -7,23 +7,43 @@
 
 import UIKit
 
-class AboutMeViewController: UIViewController {
-
+final class AboutMeViewController: UIViewController {
+    
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var surnameLabel: UILabel!
+    @IBOutlet var ageLabel: UILabel!
+    @IBOutlet var hometownLabel: UILabel!
+    @IBOutlet var photoImageView: UIImageView!
+    
+    @IBOutlet var moreDetailsButton: UIBarButtonItem!
+    @IBOutlet var aboutMeNavigationItem: UINavigationItem!
+    
+    
+    var person: Person!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        nameLabel.text = person.name
+        surnameLabel.text = person.surname
+        ageLabel.text = person.age.formatted()
+        hometownLabel.text = person.hometown
+        if let photo = person.photo, let image = UIImage(named: photo) {
+            photoImageView.image = UIImage(named: photo)
+            photoImageView.layer.cornerRadius = photoImageView.frame.width / 2
+        } else {
+            photoImageView.isHidden.toggle()
+        }
+        
+        aboutMeNavigationItem.title = "\(person.surname) \(person.name)"
+        if person.details.isEmpty {
+            moreDetailsButton.isHidden.toggle()
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        guard let detailsVC = segue.destination as? DetailsViewController else { return }
+        
+        detailsVC.details = person.details
     }
-    */
-
 }
