7e25d55d7f521c7f773519fdbd6e03d494bb0407
NikolayD <volodar86@yandex.ru>
2024-07-12 10:37:59 +0300

    create model structures for user, person and detail. create static method  to initiate new user.

--- AboutMeApp/Models/User.swift
+++ AboutMeApp/Models/User.swift
@@ -5,4 +5,51 @@
 //  Created by NikolayD on 10.07.2024.
 //
 
-import Foundation
+struct User {
+    let userName: String
+    let password: String
+    let person: Person
+    
+    static func getUser() -> User {
+        User(
+            userName: "User",
+            password: "1234",
+            person: Person(
+                name: "Николай",
+                surname: "Дворников",
+                age: 38,
+                hometown: "Москва",
+                details: [
+                    Detail(
+                        title: "Навыки",
+                        description: """
+                        Имею опыт разработки на Java в связке с Oracle EJB, \
+                        написания скриптов на bash и sql запросов. \
+                        Работал с сервером приложений Oracle Weblogic и сервером БД Oracle DB.
+                        """
+                    ),
+                    Detail(
+                        title: "Хобби",
+                        description: """
+                        Сейчас мое хобби - это изучение японского языка. \
+                        До этого увлекался дайвингом и исторической реконструкцией.
+                        """
+                    )
+                ]
+            )
+        )
+    }
+}
+
+struct Person {
+    let name: String
+    let surname: String
+    let age: Int
+    let hometown: String
+    let details: [Detail]
+}
+
+struct Detail {
+    let title: String
+    let description: String
+}
